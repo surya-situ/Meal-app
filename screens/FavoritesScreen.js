1c4136e164f015503b3ef9f7eb4e804d7c2ff6db
Suryakanta Das <suryasocial55@gmail.com>
2023-10-26 11:00:46 +0530

    favorite meal screen where a user can save his/her favorite meal

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+function FavoritesScreen () {
+    return (
+        <View>
+            <Text>Favorite screen</Text>
+        </View>
+    )
+};
+
+export default FavoritesScreen;
